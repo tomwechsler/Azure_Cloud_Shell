ddfd8560acd4d38bc7acd27348a2b9cb2bbdebe8
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2020-10-01 15:32:16 +0200

    Add files via upload

---
+++ Create_WebApp_with_Staging_Slot.sh
@@ -0,0 +1,38 @@
+az login
+
+az account show
+
+az account list --all --output table
+
+az account set --subscription "MSDN Platforms"
+
+#Replace the following URL with a public GitHub repo URL
+gitrepo=https://github.com/Azure-Samples/php-docs-hello-world
+webappname=mywebapp$RANDOM
+
+#Create a resource group.
+az group create --location westeurope --name myResourceGroup
+
+#Create an App Service plan in STANDARD tier (minimum required by deployment slots).
+az appservice plan create --name $webappname --resource-group myResourceGroup --sku S1
+
+#Create a web app.
+az webapp create --name $webappname --resource-group myResourceGroup --plan $webappname
+
+#Create a deployment slot with the name "staging".
+az webapp deployment slot create --name $webappname --resource-group myResourceGroup --slot staging
+
+#Deploy sample code to "staging" slot from GitHub.
+az webapp deployment source config --name $webappname --resource-group myResourceGroup --slot staging --repo-url $gitrepo --branch master --manual-integration
+
+#Copy the result of the following command into a browser to see the staging slot.
+echo http://$webappname-staging.azurewebsites.net
+
+#Swap the verified/warmed up staging slot into production.
+az webapp deployment slot swap --name $webappname --resource-group myResourceGroup --slot staging
+
+#Copy the result of the following command into a browser to see the web app in the production slot.
+echo http://$webappname.azurewebsites.net
+
+#Clean Up
+az group delete --name myResourceGroup --yes
